e737228b033428c375c72c9052785ceaf6428912
ankit-anklabs <70593372+ankit-anklabs@users.noreply.github.com>
2021-06-15 18:08:19 +0530

    Add files via upload

---
+++ HTS221_Temperture-Humidity_sensor/STM_HAL-Library/STM32L151xx_project_file/Core/Inc/Anklabs.h
@@ -0,0 +1,33 @@
+/**
+ ******************************************************************************
+ * @file    Anklabs.h
+ * @author  Ankit singh
+ * @brief   Anklabs header driver file
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2017 ANKLABS.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ANKLAB under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+ /** Anklabs.h
+ *
+ *  Created on: Apr 15, 2021
+ *      Author: Ankit singh
+ */
+
+#ifndef INC_ANKLABS_H_
+#define INC_ANKLABS_H_
+
+
+
+#endif /* INC_ANKLABS_H_ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "HTS221_anklabs.h"
---
+++ HTS221_Temperture-Humidity_sensor/STM_HAL-Library/STM32L151xx_project_file/Core/Inc/HAL_I2C_anklabs.h
@@ -0,0 +1,42 @@
+/**
+ ******************************************************************************
+ * @file    HAL_I2C_anklabs.h
+ * @author  R. Ankit singh
+ * @brief   HAL_I2C_anklabs header driver file
+ * @Version 1.0
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2017 ANKLABS.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ANKLAB under MIT,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/MIT
+ *
+ ******************************************************************************
+ */
+/*
+ * HAL_I2C_anklabs.h
+ *
+ *  Created on: Apr 15, 2021
+ *      Author: R. Ankit singh
+ */
+
+#include <stdio.h>
+#include "stm32l1xx_hal.h"
+
+#ifndef INC_HAL_I2C_ANKLABS_H_
+#define INC_HAL_I2C_ANKLABS_H_
+
+#endif /* INC_HAL_I2C_ANKLABS_H_ */
+
+struct array
+{
+    uint8_t arr[8];
+}a1;
+
+uint8_t HTS221_Read_i2c(uint8_t Dev_address, uint8_t Dev_addressR, uint8_t R_Address);
+void HTS221_Write_i2c(uint8_t Dev_address, uint8_t R_Address, uint8_t Data);
+HTS221_Read_multi_i2c(uint8_t Dev_address, uint8_t Dev_addressR, uint8_t R_Address, uint8_t size);
